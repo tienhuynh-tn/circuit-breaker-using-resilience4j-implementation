3c909e319fafd4b9ed1292f0ca71d3205f58c609
tienhuynh-tn <tien.huynhlt.tn@gmail.com>
2024-09-03 20:52:37 +0700

    Init source code

---
+++ src/main/java/com/tienhuynh_tn/circuit_breaker_using_resilience4j_implementation/CircuitBreakerUsingResilience4jImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.tienhuynh_tn.circuit_breaker_using_resilience4j_implementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitBreakerUsingResilience4jImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitBreakerUsingResilience4jImplementationApplication.class, args);
+	}
+
+}
